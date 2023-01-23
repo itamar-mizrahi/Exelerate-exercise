79d3eaafc51f78cabb51fc3e1c6ac22aa50a9229
Itamar Mizrahi <Itamarm@wotch.health>
2023-01-23 21:56:29 +0200

    feat added toastr ,style, url, setInterval, table sorted top 3

--- Exelerate-exercise/src/app/app.component.ts
+++ Exelerate-exercise/src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Data from './data.json';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+interface Data {
+  id: Number;
+  name: String;
+  amount: Number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Exelerate-exercise';
+  Data = Data.sort((a, b) => b.amount - a.amount);
+  data: Data[] = Data;
+  totalMoney: Number = 0;
+
+  constructor(private http: HttpClient, private toastr: ToastrService) {}
+
+  showToaster() {
+    this.http.get<any>('https://api.npms.io/v2/invalid-url').subscribe({
+      next: (data) => {
+        this.totalMoney = data.total;
+      },
+      error: (error) => {
+        this.toastr.error(`error: ${error.status}`, 'Failed');
+        console.error('There was an error!', error);
+      },
+    });
+  }
+
+  getRandomNumber() {
+    this.http
+      .get<any>('http://www.randomnumberapi.com/api/v1.0/randomnumber')
+      .subscribe((data) => {
+        this.totalMoney = data * 1000000;
+        this.toastr.success('Updated', 'Successed', {
+          // toastClass: 'toast-top-center',
+          timeOut: 1000,
+        });
+      });
+  }
+
+  ngOnInit() {
+    this.toastr.toastrConfig.positionClass = 'toast-bottom-center';
+    setInterval(() => {
+      this.getRandomNumber();
+    }, 3000);
+
+    // this.http.get<any>('https://api.npms.io/v2/invalid-url').subscribe({
+    //     next: data => {
+    //         this.totalAngularPackages = data.total;
+    //     },
+    //     error: error => {
+    //         console.error('There was an error!', error);
+
+    //     }
+    // })
+  }
 }
